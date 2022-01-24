ced8654c260ad47380248791ff612c934950d4d9
ABC-Strategie-GitHub <96920178+ABC-Strategie-GitHub@users.noreply.github.com>
2022-01-24 16:47:00 +0100

    Update __manifest__.py

--- fattura_immediata/__manifest__.py
+++ fattura_immediata/__manifest__.py
@@ -17,10 +17,29 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'account', 'sale','l10n_it_delivery_note',
+    'depends': ['base',
+                'account',
+                'sale',
+                'stock',
+                'partner_fax',
+                'abc_codice_arca',
+                'abc_codice_iva',
+                'abc_x_planethoreca',
+                'l10n_it_account',
+                'l10n_it_delivery_note',
                 'l10n_it_delivery_note_base',
                 'l10n_it_delivery_note_batch',
-                'l10n_it_delivery_note_order_link'],
+                'l10n_it_delivery_note_order_link',
+                'l10n_it_fatturapa',
+                'l10n_it_fatturapa_in',
+                'l10n_it_fatturapa_out',
+                'l10n_it_fatturapa_sale',
+                'l10n_it_fiscal_document_type',
+                'l10n_it_fiscal_payment_term',
+                'l10n_it_fiscalcode',
+                'l10n_it_payment_reason',
+                'l10n_it_pec',
+                'l10n_it_rea'],
 
     # always loaded
     'data': [
