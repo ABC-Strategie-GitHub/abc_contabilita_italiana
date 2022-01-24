851db2df7b5db54c495290ff3804be394737a058
ABC-Strategie-GitHub <96920178+ABC-Strategie-GitHub@users.noreply.github.com>
2022-01-24 16:22:26 +0100

    Update __manifest__.py

--- fattura_immediata/__manifest__.py
+++ fattura_immediata/__manifest__.py
@@ -14,7 +14,7 @@
 
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.4',
+    'version': '0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base',
